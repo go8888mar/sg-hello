fc4f88473448f155780d445ff9b09926d9744295
go8888mar <go8888mar@naver.com>
2024-09-26 13:33:01 +0900

    sg-hello first commit

---
+++ src/test/java/com/aldente/hello/SgHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldente.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
